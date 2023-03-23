df2245b6bdbcd291453fe4a9efb43cca96b7b0df
Maria Eduarda <mariaeduardasoter@gmail.com>
2023-03-23 18:55:08 -0300

    exercises 1, 2, 3, 4 and 5

--- src/cod/ex04.java
+++ src/cod/ex04.java
@@ -15,16 +15,32 @@ resolução:
 
 o espaço inicial do carro é 0.
 o espaço inicial do caminhão é 100km.
-a velocidade do carro é positiva 110km.
-a velocidade do caminhão é negativa -80km.
+a velocidade do carro 110km.
+a velocidade do caminhão 80km.
 
-funções horárias
+-no ponto de encontro os veículos tem espaços iguais
+carro = caminhão (50km = 50km)
 
-no ponto de encontro os veículos tem espaços iguais
-carro = caminhão
+-tempo que cada veículo leva para chegar ao local de encontro
+tempo = distância/velocidade
+carro: 50km, 110km/h
+ 50/110 = 0,45h | conversão para minutos:  0,45.60 = 27 minutos
+caminhão: 50km, 80km/h
+ 50/80 = 0,625h | conversão para minutos: 0,625.60 = 37,5 + 5.2(pedagio) = 47,5 minutos (47,5/60=0,791h)
+ 
+ -distância percorrida pelos veículos
+ distância = velocidade x tempo
+ carro: 110km, 0,45h
+ 110.0,45 = 49,5km
+ caminhão: 80km, 0,625h
+ 80.0,791 = 63km
 
-local do encontro
-
+ -valores finais: 
+ carro- tempo: 27 minutos | distância: 49,5km
+ caminhão- tempo: 47,5 minutos | distância: 63km
+ com isso podemos concluir que o veículo mais próximo de Ribeirão Preto é o caminhão, pois
+ ele está indo à Ribeirão Preto, o que significa que falta apenas 37km(100km - 63km = 37km) para chegar até o local, enquanto
+ o carro já se afastou 49,5km(pois ele está indo em direção à França).
  */
 
 
--- src/cod/ex05.java
+++ src/cod/ex05.java
@@ -10,12 +10,69 @@ b) Evite usar funções prontas, como, por exemplo, reverse;
 
 package cod;
 
+import java.util.Scanner;
+
 public class ex05 {
 	
 	public static void main(String[]args) {
 		
-
+		Scanner sc = new Scanner(System.in);
 		
+		String input = sc.nextLine();
+		
+		
+		char[] chars = input.toCharArray();
+		int x = chars.length - 1;
+		int y = 0;
+		
+			while(y < x) {
+			char temp = chars[y];
+			chars[y] = chars[x];
+			chars[x] = temp;
+			y++;
+			x--;
+			}
+		
+		String newstring = new String(chars);
+		System.out.println(newstring);
 	}
 
 }
+/*a string input foi é convertida em um array de char(caracteres).
+		 *x recebe chars.length - 1 que define a última posição do array(subtraindo -1 do valor total do array, obtemos a última posição).
+		 *y recebe 0 dando a ele a primeira posição do array.
+		 *
+		 *com o laço de repetição while invertemos a posição dos caracteres armazenados no array
+		 *, obtemos a seguinte lógica:
+		 *
+		 *exemplo de entrada: dado
+		 *
+		 *tamanho do array: 3
+		 *
+		 *
+		 *enquanto y < x (y=0 e x=3)
+		 *char temp = chars[y] 
+		 *chars[y] = chars[x] (caractere na posição 0 é trocado pelo caractere na posição 3)
+		 *chars [x] = temp
+		 *y++ (y+1)
+		 *x-- (x-1)
+		 *(d a d o)
+		 *(o a d d)
+		 *
+		 *enquanto y < x (y=1 e x=2)
+		 *char temp = chars[y] 
+		 *chars[y] = chars[x] (caractere na posição 1 é trocado pelo caractere na posição 2)
+		 *chars [x] = temp
+		 *y++ (y+1)
+		 *x-- (x-1)
+		 *(o a d d)
+		 *(o d a d)
+		 *
+		 *enquanto y < x (y=2 e x=1)
+		 *
+		 *
+		 *após a troca de posições dos caracteres, chars é convertido novamente em
+		 *String e é mostrado na tela a partir do system.out.println().
+		 *
+		 *
+		*/
